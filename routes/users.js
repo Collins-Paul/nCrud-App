aa0777352da2f3e49953a9491863897891ba9681
AGOGO COLLINS NYERHOVWO <58529336+Collins-Paul@users.noreply.github.com>
2024-07-31 18:19:49 +0100

    Delete Confirmation, User Profile, User Profile Edit, User Password Reset, all UI Created

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/users/all', function(req, res, next) {
-  res.render('layouts/home', { title: 'All Users'});
-});
-
-module.exports = router;
